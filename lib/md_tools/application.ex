3aa74d74ec6c719d8b7ad7913830d5b38bccd689
andyl <andy@r210.com>
2024-01-14 19:01:37 -0800

    Move supervisor under 'Fts' namespace

--- lib/md_tools/application.ex
+++ lib/md_tools/application.ex
@@ -7,7 +7,7 @@ defmodule MdTools.Application do
 
   @impl true
   def start(_type, _args) do
-      children = [MdTools.Svc.Supervisor]
+      children = [MdTools.Fts.Svc.Supervisor]
       opts = [strategy: :one_for_one, name: __MODULE__]
 
       Supervisor.start_link(children, opts)
