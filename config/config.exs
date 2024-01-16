03d9071d5765782fb8b3240867663fc3852eb5ab
andyl <andy@r210.com>
2024-01-15 19:11:05 -0800

    Add config files
    These are necessary to configure Nx components.
    
    It's not clear how Environment Variables should be passed to Nx sub-systems.

---
+++ config/config.exs
@@ -0,0 +1,20 @@
+import Config
+
+config :nx, [
+  default_backend: EXLA.Backend,
+  default_defn_options: [compiler: EXLA]
+]
+
+config :exla, [
+  default_client: :cuda,
+  clients: [
+    host: [platform: :host],
+    cuda: [platform: :cuda]
+  ]
+]
+
+System.put_env([
+  XLA_TARGET: "cuda120",
+  TF_CPP_MIN_LOG_LEVEL: 3]
+)
+
---
+++ config/runtime.exs
@@ -0,0 +1,5 @@
+System.put_env([
+  XLA_TARGET: "cuda120",
+  TF_CPP_MIN_LOG_LEVEL: 3]
+)
+
