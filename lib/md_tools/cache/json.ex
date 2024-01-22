ac443ab04564a9ea2e613cc891d6c229b5e1e3fe
andyl <andy@r210.com>
2024-01-21 22:26:33 -0800

    Add cache modules
    The 'cache' are files used to store intermediate processing results.
    
    docs -> cache -> database
    
    There are two types of cache files: data (ndjson) and vectors (parquet).

---
+++ lib/md_tools/cache/json.ex
@@ -0,0 +1,14 @@
+defmodule MdTools.Cache.Json do
+  def read(_cachefile) do
+  end
+
+  def append(_cachefile, _data) do
+  end
+
+  def update(_cachefile, _docfile, _data) do
+  end
+
+  def delete(_cachefile, _docfile) do
+  end
+
+end
---
+++ lib/md_tools/cache/manager.ex
@@ -0,0 +1,8 @@
+defmodule MdTools.Cache.Manager do
+  def sync_data do
+  end
+
+  def sync_vectors do
+  end
+
+end
