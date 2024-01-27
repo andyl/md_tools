16873cd77f0924a1f04e39ab25dcaab0c7d061ab
andyl <andy@r210.com>
2024-01-27 12:28:59 -0800

    Add test for json_stage

---
+++ test/md_tools/cache/svc/json_stage_test.exs
@@ -0,0 +1,13 @@
+defmodule MdTools.Cache.Svc.JsonStageTest do
+
+  use ExUnit.Case
+
+  alias MdTools.Cache.Svc.JsonStage
+
+  describe "#myfunc/1" do
+    test "description" do
+      assert JsonStage
+    end
+  end
+
+end
